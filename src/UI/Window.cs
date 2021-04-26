09bd435762f01f79ab12f6511038ac23fe5544c2
Chris Schubert <chris.a.schubert25@gmail.com>
2021-04-26 19:28:00 -0400

    Fixing API Key retrieval

--- src/UI/Window.cs
+++ src/UI/Window.cs
@@ -25,18 +25,31 @@ namespace Appalachia.WakaTime.UI
 
         private void OnGUI()
         {
-            _enabled = EditorGUILayout.Toggle("Enable WakaTime", _enabled);
-            _wakatimePathAuto = EditorGUILayout.ToggleLeft("WakaTime Auto Path", _wakatimePathAuto);
-            _wakatimePath = EditorGUILayout.TextField("WakaTime Path", _wakatimePath);
-            _apiKey = EditorGUILayout.TextField("API key",             _apiKey);
-            EditorGUILayout.LabelField("Project name", _projectName);
+            _needToReload = false;
 
+            _enabled = EditorGUILayout.Toggle("Enable WakaTime", _enabled);
+            
+            _apiKey = EditorGUILayout.TextField("API key", _apiKey);
+
+            EditorGUILayout.BeginHorizontal();
+            _wakatimePathAuto = EditorGUILayout.ToggleLeft("WakaTime Auto Path", _wakatimePathAuto);
+
+            GUI.enabled = !_wakatimePathAuto;
+            _wakatimePath = EditorGUILayout.TextField("WakaTime Path", _wakatimePath);
+            GUI.enabled = true;
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField("Project name", _projectName);
             if (GUILayout.Button("Change project name"))
             {
                 ProjectWindow.Display();
                 _needToReload = true;
             }
 
+            EditorGUILayout.EndHorizontal();
+            
             _debug = EditorGUILayout.Toggle("Debug", _debug);
 
             EditorGUILayout.BeginHorizontal();
@@ -49,6 +62,13 @@ namespace Appalachia.WakaTime.UI
                 EditorPrefs.SetBool(Configuration.EnabledPref, _enabled);
                 EditorPrefs.SetBool(Configuration.DebugPref,   _debug);
                 WakaTime.Initialize();
+                _needToReload = _needToReload || GUI.changed;
+            }
+
+            if (GUILayout.Button("Save Preferences"))
+            {
+                _needToReload = true;
+                CheckReloadStatus();
             }
 
             if (GUILayout.Button("Open Dashboard"))
@@ -57,34 +77,41 @@ namespace Appalachia.WakaTime.UI
             }
 
             EditorGUILayout.EndHorizontal();
+
+            CheckReloadStatus();
         }
 
         private void OnFocus()
         {
+            CheckReloadStatus();
+        }
+
+        private void CheckReloadStatus()
+        {
             if (_needToReload)
             {
+                RefreshPreferences();
                 WakaTime.Initialize();
                 _needToReload = false;
             }
+        }
 
+        private void RefreshPreferences()
+        {
             if (EditorPrefs.HasKey(Configuration.WakaTimePathAutoPref))
             {
                 _wakatimePathAuto = EditorPrefs.GetBool(Configuration.WakaTimePathAutoPref);
             }
 
-            GUI.enabled = !_wakatimePathAuto;
-            
             if (EditorPrefs.HasKey(Configuration.WakaTimePathPref))
             {
                 _wakatimePath = EditorPrefs.GetString(Configuration.WakaTimePathPref);
             }
 
-            GUI.enabled = true;
-
-            /*if (EditorPrefs.HasKey(Configuration.ApiKeyPref))
+            if (EditorPrefs.HasKey(Configuration.ApiKeyPref))
             {
                 _apiKey = EditorPrefs.GetString(Configuration.ApiKeyPref);
-            }*/
+            }
 
             if (EditorPrefs.HasKey(Configuration.EnabledPref))
             {
